14588d8575c1d54500177120d1ebb1baa3e5b10a
uotani <s.uotani.zetakansu@gmail.com>
2019-03-26 15:06:47 +0000

    create sidebar

--- app/javascript/packs/components/side_menu.vue
+++ app/javascript/packs/components/side_menu.vue
@@ -1,13 +1,143 @@
 <template>
-    <ul>
-        
-    </ul>
+  <ul v-bind:class="{toggled: isToggled}">
+    <a href="#" class="sidebar-bland">
+      <div class="sidebar-bland-text">Club Connect</div>
+    </a>
+    <hr class="sidebar-divider" />
+    <li class="nav-item">
+      <router-link :to= "{name: 'home'}" class="active">
+        <i class="fas fa-tachometer-alt"></i>
+        Dashboard
+      </router-link>
+    </li>
+    <hr class="sidebar-divider" />
+    <div class="sidebar-heading">MAIN</div>
+    <li class="nav-item">
+      <router-link :to= "{name: 'todo'}">
+        <i class="fas fa-tachometer-alt"></i>
+        List
+      </router-link>
+    </li>
+    <li class="nav-item">
+      <a href="#">
+        <i class="fas fa-tachometer-alt"></i>
+        Dashboard
+      </a>
+    </li>
+    <hr class="sidebar-divider" />
+    <div class="sidebar-heading">SETTINGS</div>
+    <li class="nav-item">
+      <a href="#">
+        <i class="fas fa-tachometer-alt"></i>
+        Dashboard
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="#">
+        <i class="fas fa-tachometer-alt"></i>
+        Dashboard
+      </a>
+    </li>
+    <hr class="sidebar-divider" />
+    <div class="text-center">
+      <button type="button" id="sidebar-toggle" v-on:click="toggleSidebar">
+        <i class="fas" v-bind:class="{'fa-chevron-left': !isToggled, 'fa-chevron-right': isToggled}"></i>
+      </button>
+    </div>
+  </ul>
 </template>
 
-<style scoped>
-    ul{
-        min-height: 100vh;
-        background-color: #4e73df;
-        width: 14rem;
+<script>
+  export default{
+    data(){
+      return{
+        isToggled: false
+      }
+    },
+    methods: {
+      toggleSidebar: function(){
+        this.isToggled = !this.isToggled
+      }
     }
+  }
+</script>
+
+<style scoped>
+  ul{
+    min-height: 100vh;
+    background-color: #4e73df;
+    width: 15rem;
+    list-style: none;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  
+  li:hover{
+    background-color: rgba(255,255,255,0.2);
+  }
+  
+  .toggled{
+    width: 6.5rem
+  }
+  .text-center{
+    text-align: center;
+  }
+  a.sidebar-bland{
+    height: 4.375rem;
+    font-size: 20px;
+    color: white;
+    display: flex;
+    text-decoration: none;
+  }
+  
+  .sidebar-bland .sidebar-bland-text{
+    display: inline;
+    align-self: center;
+    width: 100%;
+    text-align: center;
+  }
+  
+  .sidebar-heading{
+    color: rgba(255,255,255,.4);
+    padding: 0 1rem;
+    margin-top: 1rem;
+    font-size: .65rem;
+    font-weight: 800;
+    text-align: left;
+  }
+  
+  .nav-item a{
+    display: block;
+    text-align: left;
+    padding: 1rem;
+    color: rgba(255,255,255,0.8);
+    text-decoration: none;
+    font-size: 0.85rem;
+  }
+  
+  ul.toggled .nav-item a{
+    text-align: center;
+  }
+  
+  .nav-item a:hover, .nav-item a.active{
+    color: white;
+  }
+  
+  .sidebar-divider{
+    height: 0;
+    border: none;
+    margin: 0 1rem;
+    border-top: 1px solid rgba(255,255,255,.15);
+  }
+  
+  #sidebar-toggle{
+    background-color: rgba(255,255,255,.2);
+    border: none;
+    color: white;
+    padding: 0.6rem 1rem;
+    border-radius: 24px;
+    font-size: 25px;
+    margin-top: 0.3rem;
+    cursor: pointer;
+  }
 </style>
