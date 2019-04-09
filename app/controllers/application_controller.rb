2f4b60b0d4984694063462c48ac9df083cdf5358
uotani <s.uotani.zetakansu@gmail.com>
2019-04-09 01:40:53 +0000

    add error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
 
+  rescue_from StandardError, with: :notify_standard
+  rescue_from ActiveRecord::RecordNotFound, with: :render_json_404
+
   def after_sign_in_path_for(resource_or_scope)
     home_path
   end
@@ -7,5 +10,20 @@ class ApplicationController < ActionController::Base
   # ログアウト後のリダイレクト先
   def after_sign_out_path_for(resource_or_scope)
     new_team_session_path
+  end
+  
+  def render_json_404(e = nil)
+    if e
+      logger.info "Rendering 404 with exception: #{e.message}"
+    end
+  end
+
+  def notify_standard(e = nil)
+    if e
+      logger.info "Rendering 500 with exception: #{e.message}"
+      logger.error e.backtrace.join("\n")
+      notifier = Slack::Notifier.new(Rails.application.config.slack_webhook_url)
+      notifier.ping("-------------\n" + e.message + "\n" + e.backtrace.join("\n"))
+    end
   end
 end
