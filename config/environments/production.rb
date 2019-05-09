d57933d65813ca8f9579a6d3906b8fb111c3777d
uotani <s.uotani.zetakansu@gmail.com>
2019-05-09 02:21:41 +0000

    add mail setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
     :address => "smtp.gmail.com",
     :port => 587,
     :user_name => "supp.torus@gmail.com",
-    :password => "1024satox",
+    :password => "cctalzyrdlknkdhk",
     :authentication => :plain,
     :enable_starttls_auto => true
   }
